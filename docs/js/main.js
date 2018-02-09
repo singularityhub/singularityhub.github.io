cb4bbace1acd7c0a618084853df44d3efbf8235c
vsoch <vsochat@stanford.edu>
2018-02-08 17:47:28 -0800

    adding robots site wouhou!

---
+++ docs/js/main.js
@@ -0,0 +1,29 @@
+$(window).load(function() {
+    $("#preloader").fadeOut("slow");
+});
+
+$(document).ready(function(){
+
+    wow = new WOW({
+        mobile:       false,       // default
+      }
+    )
+    wow.init();
+
+     $('#top-nav').onePageNav({
+        currentClass: 'current',
+        changeHash: true,
+        scrollSpeed: 1200
+    });
+
+     
+    //animated header class
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 100) {
+            $(".navbar-default").addClass("animated");
+        } else {
+            $(".navbar-default").removeClass('animated');
+        }
+    });
+
+});
